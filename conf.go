12e3c9d8d2005add33aaddce6a110fbcb5a5490a
bagaking <zfaustk@qq.com>
2024-08-11 12:38:03 +0800

    feat (conf): Enhance file_bundle functionality and refactor code
    - Add support for creating default configuration files and directories.
    - Update file handling and error handling functions.
    - Modify the Makefile and update dependencies in go.mod and go.sum.

--- conf.go
+++ conf.go
@@ -5,16 +5,19 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/khicago/irr"
+
 	"github.com/BurntSushi/toml"
 )
 
-const defaultName = "bundle.bundle"
-const defaultConfigExt = ".file_bundle_rc"
+const (
+	defaultName      = "bundle.bundle"
+	defaultConfigExt = ".file_bundle_rc"
+)
 
 var (
 	input   string
@@ -42,30 +45,86 @@ type Config struct {
 	Description string   `toml:"description"`
 }
 
+// 如果用户执行 file_bundle -touch，则创建一个默认的配置文件或目录，然后退出
 func touch() {
-	// 如果用户执行 file_bundle init，则创建一个默认的配置文件并退出程序
-
 	defaultConf := Config{
 		Entry:   []string{"./*"},
-		Exclude: []string{".bundle"},
-		Output:  defaultName,
+		Exclude: []string{".bundle", ".bundle.txt"},
 	}
-
 	var buf bytes.Buffer
-	err := toml.NewEncoder(&buf).Encode(defaultConf)
+	// 检查是否需要创建目录
+	if flag.Arg(0) == "dir" {
+		err := toml.NewEncoder(&buf).Encode(defaultConf)
+		if err != nil {
+			exitWith("Could not encode default config: %v", err)
+		}
+		if err = createConfDir(buf.Bytes()); err != nil {
+			exitWith("Could not create directory: %v", err)
+		}
+		exitWith("bundle directory with .file_bundle_rc and Makefile created successfully.")
+	} else {
+		defaultConf.Output = defaultName
+		err := toml.NewEncoder(&buf).Encode(defaultConf)
+		if err != nil {
+			exitWith("Could not encode default config: %v\n", err)
+		}
+		fileName := "_" + defaultConfigExt
+		err = os.WriteFile(fileName, buf.Bytes(), 0o644)
+		if err != nil {
+			exitWith("Could not write default config file: %v\n", err)
+		}
+		exitWith("%s created successfully.", fileName)
+	}
+}
+
+func createConfDir(buf []byte) error {
+	err := os.Mkdir("bundle", 0o755)
 	if err != nil {
-		fmt.Printf("Could not encode default config: %v\n", err)
-		os.Exit(1)
+		return irr.Wrap(err, "could not create directory")
 	}
 
-	fileName := "_" + defaultConfigExt
-	err = os.WriteFile(fileName, buf.Bytes(), 0644)
+	err = os.WriteFile("bundle/_all"+defaultConfigExt, buf, 0o644)
 	if err != nil {
-		fmt.Printf("Could not write default config file: %v\n", err)
-		os.Exit(1)
+		return irr.Wrap(err, "could not write config file")
 	}
 
-	fmt.Printf("%s created successfully.\n", fileName)
+	makefileContent := `
+# Makefile of file_bundle
+
+# hint: To embed this makefile as a sub-cmd into the room Makefile
+#
+# bundle:
+# 	$(MAKE) -C bundle -f Makefile clean
+#	$(MAKE) -f bundle/Makefile
+
+.PHONY: all bundle clean
+
+# 获取当前目录下所有 file_bundle_rc 文件列表
+FILE_BUNDLE_RCS := $(shell find . -name '*.file_bundle_rc')
+
+# 定义后缀替换规则：将 .file_bundle_rc 结尾的文件替换为 .bundle.txt
+BUNDLES := $(FILE_BUNDLE_RCS:.file_bundle_rc=.bundle.txt)
+
+# 默认目标
+all: clean bundle
+
+# 打包
+bundle: $(BUNDLES)
+
+# 规则来生成 bundle 文件
+%.bundle.txt: %.file_bundle_rc
+	file_bundle -v -i $< -o $@
+
+# 清理
+clean:
+	rm -f $(BUNDLES)
+`
+
+	if err = os.WriteFile("bundle/Makefile", []byte(makefileContent), 0o644); err != nil {
+		return irr.Wrap(err, "could not write makefile")
+	}
+
+	return nil
 }
 
 func initConf() Config {
@@ -91,24 +150,21 @@ func initConf() Config {
 	}
 
 	if err != nil {
-		fmt.Printf("Could not get config file, make sure it exists in the current directory.\nerror= %v\n", err)
+		exitWith("Could not get config file, make sure it exists in the current directory.\nerror= %v\n", err)
 		printHelp()
-		os.Exit(1)
 	}
 
-	content, err := ioutil.ReadFile(configFileName)
+	content, err := os.ReadFile(configFileName)
 	if err != nil {
-		fmt.Printf("Could not read config file. Make sure it is present in the current directory.\nerror= %v\n", err)
+		exitWith("Could not read config file. Make sure it is present in the current directory.\nerror= %v\n", err)
 		printHelp()
-		os.Exit(1)
 	}
 
 	var config Config
 	_, err = toml.Decode(string(content), &config)
 	if err != nil {
-		fmt.Printf("Invalid config file format. It should be a valid TOML.\nerror= %v\n", err)
+		exitWith("Invalid config file format. It should be a valid TOML.\nerror= %v\n", err)
 		printHelp()
-		os.Exit(1)
 	}
 
 	// If the -o flag provided and not empty, use its value to override config.Output
@@ -141,8 +197,26 @@ func seekConfFileName() (string, error) {
 	})
 
 	if err == nil {
-		return "", fmt.Errorf("config file not found")
+		return "", irr.Error("config file not found")
 	}
 
 	return configFile, nil
+}
+
+func exitWith(format string, args ...any) {
+	f := strings.TrimSpace(format)
+	if !strings.HasSuffix(f, "\n") {
+		f += "\n"
+	}
+	hasError := false
+	for _, a := range args {
+		if _, ok := a.(error); ok {
+			hasError = true
+		}
+	}
+	if hasError {
+		f = "Error: " + f
+	}
+	fmt.Printf(f, args...)
+	os.Exit(1)
 }
--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/BurntSushi/toml v1.3.2
 	github.com/bmatcuk/doublestar v1.3.4
 )
+
+require github.com/khicago/irr v0.0.0-20240309052027-df085c2216f6 // indirect
